f07c66fb9d9df577f2c203b7b143c6c1efe157f4
nebeevaki <beevakirm@gmail.com>
2025-01-27 01:00:02 +0300

    board create

---
+++ chess/ChessEngine.py
@@ -0,0 +1,26 @@
+"""
+This class is responsible for storing all the information about the current state of the chess game.
+It will also be responsible for determining the valid moves at the current state.
+It will also keep a move log.
+"""
+
+class GameState():
+    def __init__(self):
+        #board is an 8x8 2d list, each element of the list has 2 characters.
+        #the first character represents the color of the piece, 'b' or 'w'
+        #the second character represents the type of the pice, 'K', 'Q', 'R', 'B', 'N' or 'p'
+        #"--" - represent an empty space with no piece.
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+
+        self.whiteToMove = True
+        self.moveLog = []
+
